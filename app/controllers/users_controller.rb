33be473eb85301d56f283b526841e97ad5884635
Camilla van Klinken <camillavk@gmail.com>
2017-05-24 21:39:14 +0100

    Add ability to view stripe payments on horrific profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    load_gocardless_client
-    @payments = @gocardless_client.payments
-                                  .list(params: { customer: @user.gocardless_id })
-                                  .records
+    @payments = load_payments
   end
 
   private
@@ -26,11 +23,32 @@ class UsersController < ApplicationController
     @user ||= User.find(params[:id])
   end
 
+  def load_payments
+    load_gocardless_payments + load_stripe_payments
+  end
+
   def load_gocardless_client
     @gocardless_client ||= GoCardlessPro::Client.new(
       access_token: Rails.application.secrets.gocardless_token,
       environment: :sandbox
     )
+  end
+
+  def load_gocardless_payments
+    load_gocardless_client
+    payments = @gocardless_client.payments
+                                 .list(params: { customer: @user.gocardless_id })
+                                 .records
+    gocardless_payments = []
+    payments.each { |payment| gocardless_payments << [payment, "gocardless"] }
+    gocardless_payments
+  end
+
+  def load_stripe_payments
+    payments = Stripe::Charge.list(customer: @user.stripe_token).data
+    stripe_payments = []
+    payments.each { |payment| stripe_payments << [payment, "stripe"] }
+    stripe_payments
   end
 
   def permitted_attributes
